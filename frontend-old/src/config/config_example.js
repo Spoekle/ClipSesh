efd01f0d2087ff8d0869801b5b450cb67258ccc4
Spoekle <thiangraber@gmail.com>
2025-06-02 13:29:08 +0000

    removed fallback frontend tree

--- frontend-old/src/config/config_example.js
+++
@@ -1,6 +0,0 @@
-// This is the config file for the frontend. After filling in the backend_url, rename the file to config.js
-
-const apiUrl = '';
-process.env.API_URL = apiUrl;
-
-module.exports = apiUrl;
