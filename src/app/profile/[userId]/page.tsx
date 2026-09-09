1907fc10eaa721cbec1e85e30aa7eeff1fe19730
Spoekle <thiangraber@gmail.com>
2026-09-09 17:40:43 +0200

    remove profile stuff

--- src/app/profile/[userId]/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import dynamic from 'next/dynamic';
-
-const ProfilePage = dynamic(() => import('@/views/Profile/Index'), { ssr: false });
-
-export default function UserProfilePage() {
-  return <ProfilePage />;
-}
